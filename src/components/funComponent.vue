8c6d27dc3d765872e885fa6fbbbbf52a9d91f4e7
satinder <satindersingh472@gmail.com>
2022-08-11 16:28:58 -0600

    finished

---
+++ src/components/funComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id=changing>
+    <button id="color">change color</button>
+    <div id="secrets">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    let box = document.getElementById(`secrets`);
+    let secrets = [`work hard`,`meditate`,`eat healthy`];
+    for(let i = 0; i<secrets.length;i++){
+       box[`innerHTML`] += `<p>${secrets[i]}</p>`;
+       box.addEventListener(`click`,this.color_pink); 
+    }
+    let col = document.getElementById(`color`);
+    col.addEventListener(`click`, this.change_color);
+  },
+  methods: {
+    change_color(){
+        let div = document.getElementById(`changing`);
+        div[`style`][`backgroundColor`] = `blue`;
+    },
+    color_pink(details){
+        details[`target`][`style`][`color`] = `pink`;
+    }
+  }
+};
+</script>
+
+<style scoped>
+div{
+    height:100vh;
+}
+</style>
