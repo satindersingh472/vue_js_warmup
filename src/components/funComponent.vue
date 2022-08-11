cb308d719d779036026766966c1e2b50467caf38
satinder <satindersingh472@gmail.com>
2022-08-11 17:06:14 -0600

    done with correct method

--- src/components/funComponent.vue
+++ src/components/funComponent.vue
@@ -11,13 +11,16 @@
 export default {
   mounted() {
     let box = document.getElementById(`secrets`);
-    let secrets = [`work hard`,`meditate`,`eat healthy`];
-    for(let i = 0; i<secrets.length;i++){
-       box[`innerHTML`] += `<p>${secrets[i]}</p>`;
-       box.addEventListener(`click`,this.color_pink); 
+    for(let i = 0; i< this.secrets.length;i++){
+       box[`innerHTML`] += `<p class="indiv">${this.secrets[i]}</p>`;
     }
     let col = document.getElementById(`color`);
     col.addEventListener(`click`, this.change_color);
+
+    let indiv = document.getElementsByClassName(`indiv`);
+    for(let k = 0; k<indiv.length;k++){
+      indiv[k].addEventListener(`click`,this.color_pink);
+    }
   },
   methods: {
     change_color(){
@@ -27,7 +30,12 @@ export default {
     color_pink(details){
         details[`target`][`style`][`color`] = `pink`;
     }
-  }
+  },
+  data() {
+    return {
+        secrets: [`work hard`,`meditate`,`eat healthy`],
+    };
+  },
 };
 </script>
 
